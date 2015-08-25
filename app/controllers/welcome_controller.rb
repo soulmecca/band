9386c8d92e6911c6e535706fb406b15c3c49d3fe
Philip Choi <grjkz0@gmail.com>
2015-08-25 18:32:15 -0400

    welcome page updates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
 	def index
 	end
+	def search
+		
+	end
+	def searchresults
+		@request = params['date']
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'welcome#index'
-
+  get '/search' => 'welcome#search'
+  post '/searchresults' => 'welcome#searchresults'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
